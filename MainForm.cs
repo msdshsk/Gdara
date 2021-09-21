d840f2d3d59123d1fcc9073ef615189f73b88941
Shusaku Masuda <msd.shsk@gmail.com>
2021-09-22 07:30:41 +0900

    バグを修正

--- MainForm.cs
+++ MainForm.cs
@@ -266,7 +266,7 @@ namespace ToolApp
                 }
                 finally
                 {
-                    isError = true;
+                    isError = false;
                 }
 
             }
